8b1071552236cd485962d04ca220f3c617ff33fb
Miqueias Miranda <miqueiasmiranda49@gmail.com>
2018-11-26 18:56:25 -0300

    26.11.2018

--- Aula-14-11/busca.c
+++ Aula-14-11/busca.c
@@ -22,41 +22,73 @@
 #include <string.h>
 #include <time.h>
 
-#define	count 50
+#define	count 50000
 
 void popula_vet (int* data, int a);
+
 int verifica(int* data,int a , int num);
 
 
+
+int busca_binaria(int* data, int contador, int chave );
+int busca_sequencial(int* data, int contador, int chave);
+void quickSort(int* data, int inicio, int fim);
+int particiona(int* data, int inicio, int final);
+
 int main( int argc, char** argv )
 {
-	int i;
-	srand(time(NULL));
-	int data[count];
 
-	for ( i = 0; i < count; i++)
+	srand(time(NULL)); // gera semente
+
+	int i;
+	int data[count], buscar;
+
+	clock_t inicio,final;
+
+
+	for ( i = 0; i < count; i++) // inicializa vetor
 	{	
 		data[i] = 0;
-	}// fim do for i
-	
+	}
 
-	popula_vet(data,count);
+	inicio = clock();
+	popula_vet(data,count); // executa funcao 
+	
+	final = clock();
+	printf("populacao completa\ntempo de execucao: %lu s\n", (final - inicio) / CLOCKS_PER_SEC );
 
 	if(argc > 1)
 	{
+		buscar = rand() % count;
 
 		if ( strcmp("-b",argv[1]) == 0 ) 
 		{
-			printf("binario\n\n");
+			system("clear");
+			printf("busca binaria\n");
+			printf("%d\n",buscar);
+			quickSort(data,0,count-1);
+			printf("%d\n", busca_binaria(data, count, buscar));
+			final = clock();
 
+			printf("segundos %lu \n", (final - inicio) / CLOCKS_PER_SEC );
+		
 		}else 
 		if ( strcmp("-s",argv[1]) == 0) 
 		{
-			printf("sequencial\n\n");
+			system("clear");
+			printf("sequencial\n");
+			printf("%d\n",buscar);
+			printf("%d\n", busca_sequencial(data, count, buscar));
 		}else 
 		{
 			printf("opcao invalida\n");
 		}
+	}
+	else 
+	{
+		printf("\n\nUse as seguintes opcoes\n");
+		printf(" 	-b  executa busca binaria\n");
+		printf(" 	-s  executa busca sequencial\n\n\n");
 	}
 	return 0;
 }
@@ -71,25 +103,17 @@ void popula_vet (int* data, int a)//
 		{
 			num = rand() % a;
 
-			if ( verifica(data,a,num) ) 
+			if ( verifica(data, i,num) ) 
 			{
 				data[i] = num;
 				break;
-			}else 
-			if (i == (a-1) ) 
-			{
-				break;
 			}
-		}// fim do while 
+		}
 	}
 }
 
-
-
-
-  int verifica(int* data,int a , int num) //
+  int verifica(int* data,int a , int num) // virifica se o numero ja exite
 {
-
 	int i;
 	for ( i = 0; i < a; i++)
 	{
@@ -101,9 +125,84 @@ void popula_vet (int* data, int a)//
 	return 1;
 }
 
-
-
-int busca_sequencial()
+int busca_sequencial(int* data, int contador, int chave)//
 {
+	int i;
+	 for ( i = 0; i < contador; i++)
+	 {
+	 	if (data[i] == chave)
+		{
+			return i;
+		}
+	 }
+	 return -1;
+}
+
+int busca_binaria(int* data, int contador, int chave )
+{
+	int menor, maior, meio;
+
+	menor = 0;
+
+	maior = contador - 1;
+	while (menor <= maior) 
+	{
+		meio = (maior + menor)/2;
+		if (chave < data[meio]) 
+		{
+			maior = meio -1;
+		}else 
+		if (chave > data[meio]) 
+		{
+			menor = meio + 1;
+		}else 
+		{
+			return meio;
+		}
+
+	} 
+	return -1;
+}
+
+int particiona(int* data, int inicio, int final)
+{
+	int esq, dir, pivo, aux;
+	esq = inicio;
+	dir = final;
+	pivo = data[inicio];
+
+	while (esq < dir) 
+	{
+		while (data[esq] <= pivo) 
+		{
+			esq++;
+		}
+		while (data[dir] > pivo) 
+		{
+			dir--;
+		}
+		if( esq < dir)
+		{
+			aux = data[esq];
+			data[esq] = data[dir];
+			data[dir] = aux;
+		}
+	}
+	data[inicio] = data[dir];
+	data[dir] = pivo;
+	return dir;
 
 }
+
+void quickSort(int* data, int inicio, int fim)
+{
+	int pivo;
+	if(fim > inicio)
+	{
+		pivo = particiona(data, inicio, fim);
+		quickSort(data, inicio, pivo-1);
+		quickSort(data, pivo+1, fim);
+	}	
+}
+
+
---
+++ Trabalho Final/lista 1/ex-1.c
@@ -0,0 +1,27 @@
+//==================================================================================
+//
+// Create by: Miqueias da Silva Miranda
+//
+//==================================================================================
+// Program: Exercicio 1 da lista 1
+//
+// Release:
+// 
+// 1 - 	Faca um programa que simule um controle de estoque de uma loja, onde cada 
+// 		produto, representado por meio de um resgistro, possui um identificador inteiro, nome,
+// 		quantidade e custo unitario. O programa deve deve permitir a inclusao e remocao de 
+// 		novos produtos, consulta de produtos por nome, alteracao de registros, geracao de
+// 		relatorios  
+//	
+//
+//==================================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main( int argc, char** argv )
+{	
+	
+		return 0;
+}
